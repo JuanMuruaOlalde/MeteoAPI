e28b83cff599a14168333aee6a048d4b84d0b7c3
Juan Murua Olalde <juan@susosise.es>
2022-12-26 13:25:07 +0100

    Mejorar log
    Añadiendole un par de datos

--- MeteoAPI/meteo_api/src/main/resources/static/InterfazUsuarioGUI_adaptador.js
+++ MeteoAPI/meteo_api/src/main/resources/static/InterfazUsuarioGUI_adaptador.js
@@ -1,48 +1,61 @@
-
 function ObtenerCoordenadasYMetereologia() {
-    const poblacion = document.getElementById("poblacion").value;
-    const codigoPais = document.getElementById("codigoPais").value;
+  const poblacion = document.getElementById("poblacion").value;
+  const codigoPais = document.getElementById("codigoPais").value;
 
-    fetch("http://localhost:8080/meteo", {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        }
+  fetch("http://localhost:8080/meteo", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        alert(
+          "No se ha podido obtener datos metereologicos:\n" +
+            response.status +
+            " " +
+            response.statusText
+        );
+      }
+      return response.json();
     })
-        .then((response) => {
-            if (!response.ok) {
-                alert(
-                    "No se ha podido obtener datos metereologicos:\n" +
-                        response.status +
-                        " " +
-                        response.statusText
-                );
-            }
-            return response.json();
-        })
-        .then((jsonData) => {
-            const timestamp = new Date(Date.now());
-            console.log(timestamp.toISOString(), "Consulta metereologica para [" + poblacion + " , " + codigoPais + "], con resultado: ", JSON.stringify(jsonData));
-            //_TODO_pendiente recuperarlos datos reales.
-            const temperaturaActual = jsonData["temperatura_celsius"];
-            const humedadActual = jsonData["temperatura_celsius"];
-            const vientoVelocidad = jsonData["temperatura_celsius"];
-            const vientoDireccion = jsonData["temperatura_celsius"];
-            document.getElementById("contenedorRespuestaAPI").innerHTML =
-                "<p> Temperatura: " +
-                temperaturaActual +
-                "°C </p>" +
-                "<p> Humedad: " +
-                humedadActual +
-                "% </p>" +
-                "<p> Viento: " +
-                vientoVelocidad +
-                "m/s , soplando desde " +
-                vientoDireccion +
-                "°" +
-                "</p>" +
-                "<p><small>Consulta realizada el " +
-                timestamp.toLocaleString() +
-                "</small></p>";
-        })
-        .catch((error) => alert("Se ha producido un error al obtener datos metereologicos:\n" + error.message));}
+    .then((jsonData) => {
+      const timestamp = new Date(Date.now());
+      console.log(
+        timestamp.toISOString(),
+        "Consulta metereologica para [" +
+          poblacion +
+          " , " +
+          codigoPais +
+          "], con resultado: ",
+        JSON.stringify(jsonData)
+      );
+      //_TODO_pendiente recuperarlos datos reales.
+      const temperaturaActual = jsonData["temperatura_celsius"];
+      const humedadActual = jsonData["temperatura_celsius"];
+      const vientoVelocidad = jsonData["temperatura_celsius"];
+      const vientoDireccion = jsonData["temperatura_celsius"];
+      document.getElementById("contenedorRespuestaAPI").innerHTML =
+        "<p> Temperatura: " +
+        temperaturaActual +
+        "°C </p>" +
+        "<p> Humedad: " +
+        humedadActual +
+        "% </p>" +
+        "<p> Viento: " +
+        vientoVelocidad +
+        "m/s , soplando desde " +
+        vientoDireccion +
+        "°" +
+        "</p>" +
+        "<p><small>Consulta realizada el " +
+        timestamp.toLocaleString() +
+        "</small></p>";
+    })
+    .catch((error) =>
+      alert(
+        "Se ha producido un error al obtener datos metereologicos:\n" +
+          error.message
+      )
+    );
+}
