41a4e28d0637b38b7fdf307f8457795ae88dd0b9
Juan Murua Olalde <juan@susosise.es>
2022-12-27 22:12:55 +0100

    Implementar la consulta con PaginaCompleta
    Procesando todo en el lado servidor y que este devuelva páginas completas al cliente.

--- MeteoAPI/meteo_api/src/main/java/es/susosise/meteo_api/EndpointsApp_adaptador.java
+++ MeteoAPI/meteo_api/src/main/java/es/susosise/meteo_api/EndpointsApp_adaptador.java
@@ -1,10 +1,15 @@
 package es.susosise.meteo_api;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 
 @Controller
@@ -15,15 +20,35 @@ public class EndpointsApp_adaptador {
 
     @GetMapping("/")
     public String MostrarLaPaginaInicial() {
-        return "InterfazUsuarioGUI_infraestructura";
+        return "InterfazUsuarioGUI_conAPI_infraestructura";
     }
 
-    @GetMapping("ultimasConsultas-conApi")
+    @GetMapping("/ultimasConsultas-conApi")
     public String mostrarUltimasConsultas() {
         return "UltimasConsultas_conAPI_infraestructura";
     }
 
-    @GetMapping("/ultimasConsultas")
+    @GetMapping("/PaginaCompleta")
+    public String MostrarPaginaInicial(Model model) {
+        Poblacion_dto_adaptador unaPoblacion = new Poblacion_dto_adaptador("", "ES");
+        model.addAttribute("poblacionAConsultar", unaPoblacion);
+        return "InterfazUsuarioGUI_conPaginaCompleta_infraestructura";
+    }
+
+    @PostMapping("/PaginaCompleta")
+    public String ProcesarAccionesDeLaPaginaInicial(String action,
+            @ModelAttribute("poblacionAConsultar") Poblacion_dto_adaptador poblacionAConsultar, Model model)
+            throws IOException, JSONException {
+        ParteMetereologico_dto_adaptador parte = servicios
+                .ObtenerDatosMetereologicos(poblacionAConsultar.getPoblacion(), poblacionAConsultar.getCodigoPais());
+        model.addAttribute("temperatura_celsius", parte.temperatura_celsius);
+        model.addAttribute("humedad_porcentual", parte.humedad_porcentual);
+        model.addAttribute("velocidadDelViento_ms", parte.velocidadDelViento_ms);
+        model.addAttribute("orientacionDelViento_grados", parte.orientacionDelViento_grados);
+        return "InterfazUsuarioGUI_conPaginaCompleta_infraestructura";
+    }
+
+    @GetMapping("/ultimasConsultas-conPaginaCompleta")
     public String mostrarUltimasConsultas(Model model) {
         ArrayList<ParteMetereologico_dto_adaptador> resultados = new ArrayList<>();
         ArrayList<ParteMetereologico_entidad_modelo> partes = servicios.getLos10Ultimos();
